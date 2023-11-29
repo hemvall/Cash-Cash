70a6adf79e22cbfcf6a9a5d6b676513775fe5dfc
LOUIS <louisserrano777@gmail.com>
2023-11-29 10:45:36 +0100

    add vue

---
+++ src/views/Home/HomePage.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="">
+        <Header></Header>
+    </div>
+</template>
+
+<script>
+import Header from '../../components/Header/HeaderPage.vue'
+
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    created() {
+        this.fetchData();
+    },
+    data() {
+        return {
+            user: {}
+        };
+    },
+    components: { Header },
+    methods: {
+        fetchData() {
+            this.user = {};
+            this.loading = true;
+
+            fetch(`${this.$api}/users/${localStorage.getItem('userId')}`)
+                .then(r => r.json())
+                .then(json => {
+                    this.user = json;
+                    this.loading = false;
+                    return;
+                });
+        }
+    },
+});
+</script>
+
+<style scoped></style>

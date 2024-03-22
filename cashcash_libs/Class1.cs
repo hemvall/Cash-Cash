d52d09805db5b97355897ce55a45340d051485fd
LAPTOP-C4O1BHLG\broma <bromade8300@gmail.com>
2024-03-22 16:15:10 +0100

    add thick client

---
+++ cashcash_libs/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cashcash_libs
+{
+    public class Class1
+    {
+    }
+}
